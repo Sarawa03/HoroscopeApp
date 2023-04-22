3e3f9ff1436f64d16c8e27af9299a5a403a5afb6
Sarawa03 <saraying03sb@gmail.com>
2023-04-23 01:30:17 +0200

    ui

--- app/src/main/java/com/example/horoscopeapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/horoscopeapp/ui/MainActivity.kt
@@ -1,11 +1,14 @@
-package com.example.horoscopeapp
+package com.example.horoscopeapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.horoscopeapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
 }
---
+++ app/src/main/java/com/example/horoscopeapp/ui/information/InformationFragment.kt
@@ -0,0 +1,21 @@
+package com.example.horoscopeapp.ui.information
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.horoscopeapp.R
+
+
+class InformationFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_information, container, false)
+    }
+
+
+}
